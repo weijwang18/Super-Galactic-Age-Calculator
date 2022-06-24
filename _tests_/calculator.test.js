690b96b0466d09092e8dece59fced41a4e16ec4e
weijwang18@gmail.com <weijwang18@gmail.com>
2022-06-24 11:47:05 -0700

    write first test

--- _tests_/calculator.test.js
+++ _tests_/calculator.test.js
@@ -1,7 +1,7 @@
 import Age from "./../src/js/calculator";
 
 describe("Age", () => {
-  test("should correctly return a user's age", () => {
+  test("should correctly return a user's age in Earth years", () => {
     const age = new Age(13);
     expect(age.earth).toEqual(13);
   });
