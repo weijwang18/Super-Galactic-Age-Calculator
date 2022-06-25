95663cf66cb99ec136e6e59c52fbcdc7036dd964
weijwang18@gmail.com <weijwang18@gmail.com>
2022-06-25 02:10:04 -0700

    add UI loigc to show ages on different planets

--- src/index.js
+++ src/index.js
@@ -3,5 +3,21 @@ import "bootstrap";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
+import Age from "./js/calculator";
 
-$(document).ready
+$(document).ready(function () {
+  $("form").submit(function (event) {
+    event.preventDefault();
+    const userAge = $("#ageInput").val();
+    let user = new Age(userAge);
+    let mercury = user.returnMercuryAge();
+    $(".mercury").text(mercury);
+    let venus = user.returnVenusAge();
+    $(".venus").text(venus);
+    let mars = user.returnMarsAge();
+    $(".mars").text(mars);
+    let jupiter = user.returnJupiterAge();
+    $(".jupiter").text(jupiter);
+    $(".output").show();
+  });
+});
